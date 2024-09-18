6dda972d88541805aae27743c6e2e25518200f02
Leslie Villegas <leslievillegas13@icloud.com>
2024-09-17 22:38:01 -0700

    fixed code

--- w6/main.js
+++ w6/main.js
@@ -39,11 +39,13 @@ function determineHouseHoldPts(numberInHousehold) {
 }
 
 
-function start(numberInHousehold, houseSize) {
+function start(numberInHousehold, houseSize, firstname, lastname) {
   const houseHoldPTS = determineHouseHoldPts(numberInHousehold);
   const houseSizePTS = determineHouseSizePts(houseSize);
   const total = houseHoldPTS + houseSizePTS;
   cfpData.push({
+    firstName: firstname,
+    lastName: lastname,
     houseM: numberInHousehold,
     houseS: houseSize,
     houseMPTS: houseHoldPTS,
@@ -60,7 +62,7 @@ function start(numberInHousehold, houseSize) {
     const newH2 = document.createElement("h2");
      newH2.textContent = `Carbon Footpring ${obj.cfpTotal}`;
     const newH3 = document.createElement("h3");
-    newH3.textContent = `Based on number in and size of home for ${FORM.firstname.value} ${FORM.lastname.value}`
+    newH3.textContent = `Based on number of people and size of home for ${FORM.firstname.value} ${FORM.lastname.value}`
     const newP = document.createElement("p");
     newP.textContent = `Number is based on the number of people in a house of  ${obj.houseM} (score:${obj.houseMPTS}) and a(n) ${obj.houseS}  sized home (score:${obj.houseSPTS}).`;
     OUTPUT.appendChild(newH2);
@@ -84,3 +86,4 @@ FORM.addEventListener('submit', function(e){
 
 
 // at first im messing around with the labels to see if they might be whats going on. i added the limit of 20 characters by editing the html and adding a min of 1 and a max of 20. i messed around with writing the max and it gave me the function maxlength whcih also seems to work without having to have a minimum. i added firstname and lastname to start function and it worked with displaying it in the console. im trying to add it to the obj array but im trying to see how first and last name would work since they are not objs. so i got it to work using ${FORM.firstname.value} ${FORM.lastname.value} instead of labeling them as objects. i have no idea if there are multiple ways to do this but this is what worked for me.
+// i added the names to function start() cause looking through other peoples code, i realized i forgot this
