4bea17ba1a1a6fe0f59e544bae18c96f1a33c6af
Leslie Villegas <leslievillegas13@icloud.com>
2024-09-17 22:17:03 -0700

    my attempt at the code, before I review other students code

--- w6/main.js
+++ w6/main.js
@@ -60,7 +60,7 @@ function start(numberInHousehold, houseSize) {
     const newH2 = document.createElement("h2");
      newH2.textContent = `Carbon Footpring ${obj.cfpTotal}`;
     const newH3 = document.createElement("h3");
-    newH3.textContent = `Based on number in and size of home`
+    newH3.textContent = `Based on number in and size of home for ${FORM.firstname.value} ${FORM.lastname.value}`
     const newP = document.createElement("p");
     newP.textContent = `Number is based on the number of people in a house of  ${obj.houseM} (score:${obj.houseMPTS}) and a(n) ${obj.houseS}  sized home (score:${obj.houseSPTS}).`;
     OUTPUT.appendChild(newH2);
@@ -76,8 +76,11 @@ FORM.addEventListener('submit', function(e){
   const lastName = FORM.lastname.value;
   const houseMembers = parseInt(FORM.housem.value);
   const houseSize = FORM.houses.value;
-  start(houseMembers, houseSize);
+  start(houseMembers, houseSize, firstName, lastName);
   OUTPUT.innerHTML = "";
   displayOutput();
   FORM.reset();
 })
+
+
+// at first im messing around with the labels to see if they might be whats going on. i added the limit of 20 characters by editing the html and adding a min of 1 and a max of 20. i messed around with writing the max and it gave me the function maxlength whcih also seems to work without having to have a minimum. i added firstname and lastname to start function and it worked with displaying it in the console. im trying to add it to the obj array but im trying to see how first and last name would work since they are not objs. so i got it to work using ${FORM.firstname.value} ${FORM.lastname.value} instead of labeling them as objects. i have no idea if there are multiple ways to do this but this is what worked for me.
