7f5d447f50a65e6d0dbff27e696637a742e1667b
Leslie Villegas <leslievillegas13@icloud.com>
2024-11-06 18:57:15 -0800

    setup

---
+++ w13/main.js
@@ -0,0 +1,41 @@
+const FORM = document.getElementById("form");
+const output = document.getElementById("output");
+output.style.textAlign = "center"
+const newH1 = document.createElement("h1");
+const newH2 = document.createElement("h2");
+const newH3 = document.createElement("h3");
+
+FORM.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const exerciseType = FORM.exerciseType.value;
+    let exerciseT;
+    if (exerciseType === "pull-ups") {
+      exerciseT = "pull-ups";
+    } else if (exerciseType === "sit-ups") {
+      exerciseT = "sit-ups";
+    } else if (exerciseType === "lunges") {
+      exerciseT = "lunges";
+    } else if (exerciseType === "squats") {
+        exerciseT = "squats";
+    } else if (exerciseType === "planks") {
+        exerciseT = "planks";
+    }
+
+    const repNumber = parseInt(FORM.repNumber.value);
+    const repTime = parseInt(FORM.repTime.value);
+    output.innerHTML = "";
+    newH1.textContent = `Start ${exerciseType} <> Goal is ${repNumber} reps <>`;
+    output.appendChild(newH1);
+
+    setTimeout(() => {
+        newH2.textContent = `It should take ${repTime} Minutes`;
+        output.appendChild(newH2);
+    }, repTime * 1000);
+
+    setTimeout(() => {
+        newH3.textContent = `Stop ${exerciseType}!`;
+        output.appendChild(newH3);
+    }, repTime * 5000);
+
+    FORM.reset();
+});
