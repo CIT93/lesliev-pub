0aa8114108b3a3398ce0ffe543c92aaca2408870
Leslie Villegas <leslievillegas13@icloud.com>
2024-11-14 23:44:00 -0800

    w14 my attempt at the code, before I review other students or solution code

---
+++ w14/main.js
@@ -0,0 +1,32 @@
+async function getUsers() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        if(!data.length) {
+            console.log("Error with user value") 
+        } else{
+            onSuccess(data);
+        }
+    }
+    catch (error) {
+        console.log(`Error: ${error}`)
+    }
+}
+
+getUsers();
+
+function renderUsers(data) {
+    data.forEach((user) => {
+        const usersEl = document.createElement("h1")
+        usersEl.textContent = (`User ${user.name} 's,  email is ${user.email}`)
+        document.getElementById("output").appendChild(usersEl)
+    })
+}
+
+function onSuccess(data){
+    renderUsers(data);
+}
+
+function onError(error) {
+    console.log(`Error ${error}`);
+}
